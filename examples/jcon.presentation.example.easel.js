e39dbac896a3e4a2d5d7de356a0fa4a03bcb67b4
Joshua Smith <me@joshuapaulsmith.net>
2014-02-12 15:05:08 -0800

    created prototype for the code examples

---
+++ examples/jcon.presentation.example.easel.js
@@ -0,0 +1,35 @@
+console.log('Here');
+jQuery(document).ready(function(){
+
+	var stage;
+	var canvas;
+	var preload;
+
+	function init() {
+				
+		canvas = document.getElementById("easelJS");
+		stage = new createjs.Stage(canvas);
+
+		stage.update();
+
+		// enable hover events
+		stage.enableMouseOver();
+		
+		var g = new createjs.Graphics();
+			g.setStrokeStyle(1);
+		    
+		    g.beginStroke(createjs.Graphics.getRGB(0,0,0));
+		    g.beginFill(createjs.Graphics.getRGB(255,0,0));
+		    g.drawCircle(100,100,100);
+
+		    var s = new createjs.Shape(g);
+		    s.x = 100;
+		    s.y = 100;
+
+		    stage.addChild(s);
+		    stage.update();		
+		}
+	
+
+	init();
+});
